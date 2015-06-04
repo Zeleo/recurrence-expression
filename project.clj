5c76a49646b900a4718181a0e8cd2e6cf9f6ebe8
Michi Oshima <michi.oshima@bjondinc.com>
2015-06-04 09:53:36 -0400

    Updated project information

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject recurrence-expression "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject com.bjondinc/recurrence-expression "0.1.0-SNAPSHOT"
+  :description "JSON for expressing recurrence patterns"
+  :url "https://github.com/Bjond/recurrence-expression"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
@@ -8,4 +8,8 @@
                  [clj-time "0.8.0"]
                  [prismatic/schema "0.4.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.quartz-scheduler/quartz "2.2.1"]])
+                 [org.quartz-scheduler/quartz "2.2.1"]]
+  :deploy-repositories [["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+                                     :creds :gpg}
+                         "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
+                                      :creds :gpg}]])
