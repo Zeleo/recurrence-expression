4767239841d48def8019a868d2276059df060d27
Michi Oshima <michi.oshima@bjondinc.com>
2015-07-07 17:08:51 -0400

    Added license headers

--- src/recurrence_expression/core.clj
+++ src/recurrence_expression/core.clj
@@ -1,3 +1,20 @@
+;; Copyright (c) 2015 Bjönd, Inc.
+;;
+;; This file is part of Recurrence Expression.
+;;
+;; Recurrence Expression is free software: you can redistribute it
+;; and/or modify it under the terms of the GNU Lesser General Public
+;; License as published by the Free Software Foundation, either
+;; version 3 of the License, or (at your option) any later version.
+;;
+;; Foobar is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
 (ns recurrence-expression.core
   (:require clojure.core)
   (:require [clojure.pprint :as pp])
