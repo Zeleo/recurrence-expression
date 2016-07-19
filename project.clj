a727dd0647560e865a8789f155d64599ebc9cffb
Michi Oshima <michi.oshima@bjondinc.com>
2016-07-18 20:49:45 -0400

    version to 0.2.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.bjondinc/recurrence-expression "0.2.4"
+(defproject com.bjondinc/recurrence-expression "0.2.5"
   :description "JSON for expressing recurrence patterns"
   :url "https://github.com/Bjond/recurrence-expression"
   :scm "https://github.com/Bjond/recurrence-expression.git"
