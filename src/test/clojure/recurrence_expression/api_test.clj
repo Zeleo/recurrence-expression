1b55a1db606539523e707ffc66791e8112ab2ab4
Michi Oshima <michi.oshima@bjondinc.com>
2016-08-29 10:52:52 -0400

    Commented out broken and unneeded unit test

--- src/test/clojure/recurrence_expression/api_test.clj
+++ src/test/clojure/recurrence_expression/api_test.clj
@@ -15,66 +15,74 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with Recurrence Expression.  If not, see
 ;; <http://www.gnu.org/licenses/>.
+(comment
 
-(ns recurrence-expression.api-test
-  (:require [clojure.test :refer :all]
-            [recurrence-expression.core :refer :all]
-            [recurrence-expression.data :refer :all]
-            [clj-time.core :as t])
-  (:import (clojure.lang ExceptionInfo))
-  (:import (com.bjondinc RecurrenceExpression)))
+  ;; Circa mid-August 2016, I disabled AOT compilation.
+  ;; So the RecurrenceExpression java class isn't
+  ;; available for unit testing.
+  ;; As we don't currently use that class in production,
+  ;; I'm just going to comment out the test.
+
+  (ns recurrence-expression.api-test
+    (:require [clojure.test :refer :all]
+              [recurrence-expression.core :refer :all]
+              [recurrence-expression.data :refer :all]
+              [clj-time.core :as t])
+    (:import (clojure.lang ExceptionInfo))
+    (:import (com.bjondinc RecurrenceExpression)))
 
 ;;; Note, I'm simply testing here if the calls to java wrapper
 ;;; succeeds or not.
-
-(deftest test-java-api-basic
-  (let [schedule
-        "{ \"at\": { \"day\": 5 } }"
-        start-time (t/date-time 2015 4)
-        end-time (t/date-time 2015 7)
-        current-time (t/plus start-time (t/seconds 1))]
-    
-    (let [expected-time (t/date-time 2015 4 5)]
-      (let [result (RecurrenceExpression/nextTime current-time
-                                                  schedule)]
-        (is (= expected-time
-               result)))
+  
+  (deftest test-java-api-basic
+    (let [schedule
+          "{ \"at\": { \"day\": 5 } }"
+          start-time (t/date-time 2015 4)
+          end-time (t/date-time 2015 7)
+          current-time (t/plus start-time (t/seconds 1))]
       
-      (let [result (RecurrenceExpression/nextTime current-time
-                                                  schedule
-                                                  start-time)]
-        (is (= expected-time
-               result)))
-      
-      (let [result (RecurrenceExpression/nextTime current-time
-                                                  schedule
-                                                  start-time
-                                                  end-time)]
-        (is (= expected-time
-               result)))
-      )
-
-    (let [expected-times [(t/date-time 2015 4 5)
-                          (t/date-time 2015 5 5)]]
-      (let [result (RecurrenceExpression/nextNTimes current-time
+      (let [expected-time (t/date-time 2015 4 5)]
+        (let [result (RecurrenceExpression/nextTime current-time
+                                                    schedule)]
+          (is (= expected-time
+                 result)))
+        
+        (let [result (RecurrenceExpression/nextTime current-time
                                                     schedule
-                                                    2)]
-        (is (= expected-times
-               result)))
-      
-      (let [result (RecurrenceExpression/nextNTimes current-time
-                                                    schedule
-                                                    2
                                                     start-time)]
-        (is (= expected-times
-               result)))
-      
-      (let [result (RecurrenceExpression/nextNTimes current-time
+          (is (= expected-time
+                 result)))
+        
+        (let [result (RecurrenceExpression/nextTime current-time
                                                     schedule
-                                                    2
                                                     start-time
                                                     end-time)]
-        (is (= expected-times
-               result)))
-      )
-    ))
+          (is (= expected-time
+                 result)))
+        )
+
+      (let [expected-times [(t/date-time 2015 4 5)
+                            (t/date-time 2015 5 5)]]
+        (let [result (RecurrenceExpression/nextNTimes current-time
+                                                      schedule
+                                                      2)]
+          (is (= expected-times
+                 result)))
+        
+        (let [result (RecurrenceExpression/nextNTimes current-time
+                                                      schedule
+                                                      2
+                                                      start-time)]
+          (is (= expected-times
+                 result)))
+        
+        (let [result (RecurrenceExpression/nextNTimes current-time
+                                                      schedule
+                                                      2
+                                                      start-time
+                                                      end-time)]
+          (is (= expected-times
+                 result)))
+        )
+      ))
+  )
